03fd0f1b5b97ac42016c00c45970d70e824cb0cd
Sparrow97 <vanya1228@yandex.ru>
2018-09-17 15:46:59 +0300

    Add files via upload

---
+++ JavaSwepeer/JavaSwepeer/src/sweeper/Game.java
@@ -0,0 +1,22 @@
+package sweeper;
+
+public class Game
+{
+    Matrix bombMap;
+    public Game (int cols, int rows)
+    {
+        Ranges.setSize(new Coord(cols, rows));
+    }
+
+    public void start()
+    {
+        bombMap = new Matrix(Box.ZERO);
+        bombMap.set(new Coord(0 ,0), );
+    }
+    public Box getBox(Coord coord)
+    {
+        return bombMap.get(coord);
+
+    }
+
+}
